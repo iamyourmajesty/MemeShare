5de22776326900175e822955688e40f32190afeb
Prince Pandey <princepandey2784@gmail.com>
2022-01-24 10:52:52 +0530

    load meme page is created using volley

--- app/src/main/java/com/example/memeshare/MainActivity.kt
+++ app/src/main/java/com/example/memeshare/MainActivity.kt
@@ -3,13 +3,49 @@ package com.example.memeshare
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        loadMeme()
     }
 
-    fun nextMeme(view: View) {}
-    fun shareMeme(view: View) {}
+   private fun loadMeme() {
+        // Instantiate the RequestQueue.
+        val queue = Volley.newRequestQueue(this)
+        val url = "https://meme-api.herokuapp.com/gimme"
+
+// Request a string response from the provided URL.
+        val jsonObjectRequest = JsonObjectRequest(
+            Request.Method.GET, url,null,
+            Response.Listener { response ->
+                val urL=response.getString("url")
+                Glide.with(this,).load(urL).into(imageView)
+
+
+            },
+            Response.ErrorListener {
+                Toast.makeText(this,"somethingg went wrong",Toast.LENGTH_SHORT).show()
+            })
+
+// Add the request to the RequestQueue.
+        queue.add(jsonObjectRequest)
+
+    }
+    fun nextMeme(view: View) {
+
+    }
+    fun shareMeme(view: View) {
+
+    }
 }
